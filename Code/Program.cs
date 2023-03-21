2c8065c74f153e11ab5264f383c8675826c8d4c0
Nattick <natalia.a.konovalova@gmail.com>
2023-03-21 13:37:21 +0300

    Add count

--- Code/Program.cs
+++ Code/Program.cs
@@ -5,3 +5,15 @@
 
 String[] array1 = new string[5] {"hello", "my", "name", "is", "Nattick"};
 String[] array2 = new string[array1.Length];
+void SecondArrayWithIf(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0, i < array1.Length; i++)
+    {
+    if(array[i].Length <= 3)
+    {
+        array2[count] = array1[i];
+        count++;
+    }
+    }
+}
